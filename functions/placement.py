d12b1efbe1b2c89b9fa6553701bee8ab70e90025
robyvanboxelaere <robyvboxelaere@gmail.com>
2023-06-24 16:07:02 +0200

    Added changes to folder structure and added loop to check if round or game has ended.

--- functions/placement.py
+++ functions/placement.py
@@ -8,9 +8,12 @@ monkeyDict = {
 
 def select_monkey(monkey):
     try:
-        print(monkeyDict[monkey])
-    except:
+        # Press the hotkey for the monkey
+        pyautogui.keyDown(monkeyDict[monkey])
+        # click_random()
+    except Exception as e:
         print("Whoops, something went wrong. (the monkey is not in the dictionary)")
+        print(e)
 
 #this fucntion checks every pixel around coordinates x and y for specific rgb values
 #def 
@@ -20,6 +23,7 @@ def check_area(x, y):
         for j in range(0, 100):
             if (pyautogui.pixelMatchesColor(x + i, y + j, (255, 0, 0), tolerance=0)):
                 return False
+    print("Area is valid")
     return True
 
 def click_random():
@@ -29,9 +33,11 @@ def click_random():
         y = random.randint(0, 1080)
         pyautogui.moveTo(x, y)
         if check_area(x, y):
+            print("Clicking at: " + str(x) + ", " + str(y))
             pyautogui.click(x, y)
             c = False
         
 
-    
-select_monkey("theZoo")
+# Wait 3 seconds before starting
+time.sleep(3)
+select_monkey("dartMonkey")
--- functions/vision.py
+++ functions/vision.py
@@ -4,8 +4,17 @@ import pytesseract
 import numpy as np
 from mss import mss
 import cv2
+from pyautogui import locateOnScreen
+import time
+import os
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# Get the current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the image file
+image_path = os.path.join(current_dir, "../assets/")
 
 def mse(img1, img2):
     if img1.shape != img2.shape:
@@ -29,9 +38,6 @@ def read_cash():
     sct_image = sct.grab(boundingBox)
     img = Image.frombytes('RGB', sct_image.size, sct_image.rgb)
 
-    # Save image    
-    # img.save('money.png')
-
     # Convert to grayscale
     img = img.convert('L')
 
@@ -41,9 +47,6 @@ def read_cash():
         for j in range(img.size[1]):
             if pixels[i, j] <= 254:
                 pixels[i, j] = 0
-
-    # Save grayscale image
-    # img.save('money_converted.png')
 
     # Convert to text
     cashAmount = pytesseract.image_to_string(img)
@@ -66,13 +69,13 @@ def view_round_progression():
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Read round_end.png
-    roundEnd = cv2.imread('round_end.png')
+    roundEnd = cv2.imread(os.path.join(image_path, 'round_end.png'))
     # Resize roundEnd to match img
     roundEnd = cv2.resize(roundEnd, (img.shape[1], img.shape[0]))
     roundEnd = cv2.cvtColor(roundEnd, cv2.COLOR_BGR2GRAY)
 
     # Read round_in_progress.png
-    roundInProgress = cv2.imread('round_in_progress.png')
+    roundInProgress = cv2.imread(os.path.join(image_path, 'round_in_progress.png'))
     # Resize roundInProgress to match img
     roundInProgress = cv2.resize(roundInProgress, (img.shape[1], img.shape[0]))
     roundInProgress = cv2.cvtColor(roundInProgress, cv2.COLOR_BGR2GRAY)
@@ -81,10 +84,49 @@ def view_round_progression():
     mseValEnd = mse(img, roundEnd)
     mseValInProgress = mse(img, roundInProgress)
 
+    # Return True if round ended, False if round in progress
     if mseValEnd < mseValInProgress:
         print('Round ended')
+        return True
     else:
         print('Round in progress')
-    
-view_round_progression()
-print(read_cash())
+        return False
+
+# Function that checks if the game has ended (victory or defeat screen in middle of screen)
+def check_game_state():
+    # Check if it detects victory or defeat screen image
+    # If victory
+    if locateOnScreen(os.path.join(image_path, 'victory_screen.png'), confidence=0.9) != None:
+        print('Victory')
+        return True
+    # If defeat
+    elif locateOnScreen(os.path.join(image_path, 'defeat_screen.png'), confidence=0.9) != None:
+        print('Defeat')
+        return True
+
+    print('Game still in progress')
+    return False
+
+# Function that checks round progression and cash amount
+def return_game_info():
+    return view_round_progression(), read_cash()
+
+# Loop that checks game states
+def loop_game_info(seconds):
+    # Keep looping until round ends
+    while True:
+        # Check round progression and cash amount
+        roundEnded, cashAmount = return_game_info()
+        print('Cash amount: ' + cashAmount)
+        if roundEnded:
+            break
+
+        # Check game state
+        gameEnded = check_game_state()
+        if gameEnded:
+            break
+
+        # Wait for x seconds
+        time.sleep(seconds)
+
+loop_game_info(5)
